1a8d07ab503095f8a550016c6b17da7f4d962ac2
Sergei Kompanietc <delightvlg@gmail.com>
2021-05-07 10:42:50 +0300

    feat: ex.2.1 completed

---
+++ exercises/2.1.LoopingATriangle.js
@@ -0,0 +1,11 @@
+/**
+ * Задание: Напишите цикл, который за n вызовов console. log выводит реугольник.
+ * @constructor
+ * @param {number} rows - количество строк (высота треугольника)
+ */
+
+const makeATriangle = (rows) => {
+  for (let triangle = '#'; triangle.length < rows; triangle += '#') console.log(triangle);
+}
+
+makeATriangle(8);
