69043035ac203982586024cb8ecfbe79da19427e
Sergei Kompanietc <delightvlg@gmail.com>
2021-05-18 22:29:21 +0300

    feat: added TS structure

--- exercises/2.1.LoopingATriangle.js
+++
@@ -1,8 +0,0 @@
-/**
- * Задание: Напишите цикл, который за n вызовов console. log выводит треугольник.
- */
-var makeATriangle = function (rows) {
-    for (var triangle = '#'; triangle.length < rows; triangle += '#')
-        console.log(triangle);
-};
-makeATriangle(8);
