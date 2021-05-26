ea4001332039fc525fcaaab80bce799a810b3185
Sergei Kompanietc <delightvlg@gmail.com>
2021-05-26 14:07:06 +0300

    4.4.ex completed

---
+++ exercises/4.4.DeepComparison.ts
@@ -0,0 +1,32 @@
+const deepEqual = (a: any, b: any): boolean => {
+    if (a === b) return true;
+
+    if (a === null || typeof a != 'object' ||
+    b === null || typeof b != 'object') return false;
+
+    let keysA: any[] = Object.keys(a);
+    let keysB: any[] = Object.keys(b);
+
+    if (keysA.length !== keysB.length) return false;
+
+    for (let key of keysA) {
+        if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    }
+
+    return true;
+}
+
+
+const obj1 = {
+    'a': 1,
+    'b': 2
+}
+
+// const obj2 = obj1;
+
+const obj2 = {
+    'a': 1,
+    'b': 2
+}
+
+console.log(deepEqual(obj1, obj2));
